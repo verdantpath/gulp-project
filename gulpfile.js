979538a5d3e1616c212068c0ea2db5d0a1af7b0e
verdantpath <m_dittmann@yahoo.com>
2019-01-25 22:18:27 -0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 
 gulp.task("concatScripts", function(done) {
   gulp.src([
@@ -22,6 +23,13 @@ gulp.task('minifyScripts', function(done) {
     .pipe(rename('app.min.js'))
     .pipe(gulp.dest('js'));
     done();
+});
+
+gulp.task('compileSass', function(done) {
+  gulp.src('scss/application.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('css'));
+  done();
 });
 
 // gulp.task("default", ["hello"], function() {
