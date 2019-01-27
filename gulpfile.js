950f43491244c3054d6d9c18ff743a4a90fd15e3
verdantpath <m_dittmann@yahoo.com>
2019-01-26 16:24:09 -0800

    add build task

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,20 @@ var sass = require('gulp-sass');
 var maps = require('gulp-sourcemaps');
 
 gulp.task("concatScripts", function(done) {
-  gulp.src([
+  return gulp.src([
     'js/jquery.js',
     'js/sticky/jquery.sticky.js',
     'js/main.js'
   ])
+  .pipe(maps.init())
   .pipe(concat('app.js'))
+  .pipe(maps.write('./'))
   .pipe(gulp.dest('js'));
   done();
 });
 
-gulp.task('minifyScripts', function(done) {
-  gulp.src('js/app.js')
+gulp.task('minifyScripts', ['concatScripts'], function(done) {
+  return gulp.src('js/app.js')
     .pipe(uglify())
     .pipe(rename('app.min.js'))
     .pipe(gulp.dest('js'));
@@ -27,11 +29,15 @@ gulp.task('minifyScripts', function(done) {
 });
 
 gulp.task('compileSass', function(done) {
-  gulp.src('scss/application.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('css'));
-  done();
+  return gulp.src('scss/application.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('css'));
+    done();
 });
+
+gulp.task('build', ['minifyScripts', 'compileSass']);
 
 // gulp.task("default", ["hello"], function() {
 //   console.log("This is the default task!");
