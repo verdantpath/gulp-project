cd30fa11db911c485ce6e03751be6cb45efc79c6
verdantpath <m_dittmann@yahoo.com>
2019-01-27 17:28:43 -0800

    changed gulp version to 3.9.1, add watchSass function, add clean function, add output to dist directory with original folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,9 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var maps = require('gulp-sourcemaps');
+var del = require('del');
 
-gulp.task("concatScripts", function(done) {
+gulp.task("concatScripts", function() {
   return gulp.src([
     'js/jquery.js',
     'js/sticky/jquery.sticky.js',
@@ -17,27 +18,42 @@ gulp.task("concatScripts", function(done) {
   .pipe(concat('app.js'))
   .pipe(maps.write('./'))
   .pipe(gulp.dest('js'));
-  done();
+  // done();
 });
 
-gulp.task('minifyScripts', ['concatScripts'], function(done) {
+gulp.task('minifyScripts', ['concatScripts'], function() {
   return gulp.src('js/app.js')
     .pipe(uglify())
     .pipe(rename('app.min.js'))
     .pipe(gulp.dest('js'));
-    done();
+    // done();
 });
 
-gulp.task('compileSass', function(done) {
+gulp.task('compileSass', function() {
   return gulp.src('scss/application.scss')
     .pipe(maps.init())
     .pipe(sass())
     .pipe(maps.write('./'))
     .pipe(gulp.dest('css'));
-    done();
+    // done();
 });
 
-gulp.task('build', ['minifyScripts', 'compileSass']);
+gulp.task('watchSass', function() {
+  gulp.watch('scss/**/*.scss', ['compileSass']);
+});
+
+gulp.task('clean', function() {
+  del('dist');
+});
+
+// gulp.task('build', gulp.series('concatScripts', 'minifyScripts', 'compileSass'));
+gulp.task('build', ['minifyScripts', 'compileSass'], function() {
+  return gulp.src(['css/application.css', 'js/app.min.js', 'index.html', 'img/**', 'fonts/**'], {base:'./'})
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task("default", ["build"]);
 
 // gulp.task("default", ["hello"], function() {
 //   console.log("This is the default task!");
